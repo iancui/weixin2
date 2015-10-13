5a286a972bfaf50c7685efade01bb1cba0df8e98
iancui <232747379@qq.com231>
2015-10-13 21:41:30 +0800

    add xmpp
    add xmpp

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, “8.0”
+pod “XMPPFramework”, “~> 3.6.6”
---
+++ wxxmpp.h
@@ -0,0 +1,15 @@
+//
+//  wxxmpp.h
+//  weixin
+//
+//  Created by Ian on 15/10/12.
+//  Copyright © 2015年 AppCode. All rights reserved.
+//
+
+#ifndef wxxmpp_h
+#define wxxmpp_h
+
+
+#endif /* wxxmpp_h */
+
+#import "XMPP.h"
